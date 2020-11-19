776952b712eff507d89785d2942e508bebe8646f
eMatheus23 <eliasmatheus@hotmail.com>
2020-11-19 12:02:51 -0300

    Nova rota para página conta premium visitante

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Plans from './pages/Plans';
+import CreatePremiumVisit from './pages/CreatePremiumVisit';
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path='/plans' component={Plans} />
+        <Route path='/create-premium/visit' component={CreatePremiumVisit} />
       </Switch>
     </BrowserRouter>
   );
