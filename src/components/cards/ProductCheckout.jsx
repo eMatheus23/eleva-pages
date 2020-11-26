267f5bfcba111c5d18821d2b72bc5c2599da6e83
eMatheus23 <eliasmatheus@hotmail.com>
2020-11-25 22:28:48 -0300

    Criado componente para o card de produto
    Este é o card que renderiza os pedidos que estão no carrinho

---
+++ src/components/cards/ProductCheckout.jsx
@@ -0,0 +1,72 @@
+import React, {useEffect, useState} from 'react';
+// import { Link } from 'react-router-dom';
+
+import '../../styles/components/cards/product-checkout.css'
+
+// Mockups 
+import ProductImg from '../../images/mockups/product-checkout-mockup.png'
+
+// Icons
+import deleteIcon from '../../images/icons/delete-product-checkout.svg';
+
+// Components
+import { ButtonLight } from '../Buttons'
+
+export default function ProductCheckout(props) {
+  document.title = "Elevagro | Obrigado pela compra!"
+
+  const { name, discription, price_original, price, discount } = props.product;
+  const [ priceArray, setPriceArray ] = useState([]);
+  const [ checkoutSucess, setCheckoutSucess ] = useState(false)
+
+  useEffect(() => {
+    const teste = price.split(',');
+
+    setPriceArray(teste);
+  }, [price]);
+
+  useEffect(() => {
+    setCheckoutSucess(props.success);
+  }, []);
+  
+
+  return (
+    <div className="product-checkout-card">
+      <img src={ProductImg} alt="Plataforma Elevagro"/>
+
+      <div className="product-title">
+        <h2>{name}</h2>
+        <p>{discription}</p>
+      </div>
+
+      {!checkoutSucess
+        && (
+          <>
+            <div className="product-price">
+              <p>R$: {price_original}</p>
+              <h2 className="price-style" >
+                <span>R$</span><strong>{priceArray[0]}</strong>,{priceArray[1]}
+              </h2>
+              <span>{discount} de desconto</span>
+            </div>
+
+            <img 
+              className="delete-button"
+              src={deleteIcon} 
+              onClick={() => { props.deleteProduct(props.product.id) }} 
+              alt="Excluir produto"
+            />
+          </>
+        )
+      }
+
+      {checkoutSucess
+        && (
+          <ButtonLight >
+            Acessar
+          </ButtonLight >
+        )
+      }
+    </div>
+  )
+}
