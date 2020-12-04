296aaa19f64755eba5a009e1d86627faa980d871
eMatheus23 <eliasmatheus@hotmail.com>
2020-12-03 22:25:56 -0300

    Criado um arquivo para os países do form

---
+++ src/data/countries.js
@@ -0,0 +1,19 @@
+const countryOptions = [
+  {
+    value: 'brazil',
+    label: 'Brasil',
+    flag: require('../images/flags/brazil.svg'),
+  },
+  {
+    value: 'spain',
+    label: 'Spain',
+    flag: require('../images/flags/spain.svg'),
+  },
+  {
+    value: 'usa',
+    label: 'USA',
+    flag: require('../images/flags/usa.svg'),
+  },
+];
+
+export default countryOptions;
