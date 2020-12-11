29f9bad9c8d9ae01588f8891bb89852b2ee51b28
eMatheus23 <eliasmatheus@hotmail.com>
2020-12-11 17:32:22 -0300

    Criado componente para a pesquisa pós compra
    O card de pesquisa, agora tem duas telas, de acordo com o design

--- src/pages/CheckoutAccessPage.jsx
+++ src/pages/CheckoutAccessPage.jsx
@@ -3,24 +3,12 @@ import { Link } from 'react-router-dom';
 
 //CSS
 import '../styles/pages/checkout-page.css';
-import '../styles/components/cards/complete-profile.css';
 
 import logoElevagroFooter from '../assets/images/logos/marca-elevagro.svg';
 
 // Components
-import ProgressBar from '../components/ProgressBar';
-import ButtonRounded from '../components/Buttons';
-import OptionsGenerator from '../components/SelectorOptionsGenerator';
 import ProductCheckout from '../components/cards/ProductCheckout';
-
-import ProfileIcon from '../assets/images/icons/complete-profile.svg';
-
-import {
-  professions,
-  formations,
-  cultures,
-  areas,
-} from '../data/research-options';
+import CompleteProfileCard from '../components/cards/CompleteProfile';
 
 export default function CheckoutAccessPage() {
   document.title = 'Acesse seu produto!';
@@ -28,27 +16,6 @@ export default function CheckoutAccessPage() {
   // Busca os produtos no carrinho
   const purchases = JSON.parse(sessionStorage.getItem('@elevagro-app/purchases'));
   const productsInCart = purchases;
-
-  function handleSelectChange(event) {
-    const select = event.target;
-    const input = event.target.nextSibling;
-
-    if (select.value === 'outro') {
-      select.style.display = 'none';
-      input.style.display = 'flex';
-    }
-  }
-
-  function handleInputCancel(event) {
-    const inputContainer = event.target.parentNode;
-    const input = event.target.parentNode.firstChild;
-    const select = event.target.parentNode.previousSibling;
-
-    inputContainer.style.display = 'none';
-    input.value = '';
-    select.style.display = 'flex';
-    select.value = 0;
-  }
 
   return (
     <div id='page-checkout'>
@@ -60,110 +27,7 @@ export default function CheckoutAccessPage() {
 
       <div className='content-wrapper'>
         <main>
-          <div className='complete-profile-wrapper'>
-            <div className='complete-profile-title'>
-              <h3>Compra realizada com sucesso.</h3>
-            </div>
-            <div className='card-container'>
-              <h2>Melhore a sua experiência.</h2>
-              <p>
-                A Elevagro sugere conteúdos mais úteis e relevantes para você,
-                se você tiver o seu perfil mais completo.
-              </p>
-              <form action=''>
-                <fieldset>
-                  <label htmlFor='user-prefered-culture'>
-                    Principal cultura de interesse
-                  </label>
-                  <select
-                    name='user-prefered-culture'
-                    id='user-prefered-culture'
-                    className='select-appearance'
-                    defaultValue={0}
-                    required
-                  >
-                    <option value={0} disabled>
-                      Selecione aqui
-                    </option>
-                    <OptionsGenerator array={cultures} />
-                  </select>
-                </fieldset>
-
-                <fieldset>
-                  <label htmlFor='user-prefered-area'>
-                    Principal área de interesse
-                  </label>
-                  <select
-                    name='user-prefered-area'
-                    id='user-prefered-area'
-                    className='select-appearance'
-                    defaultValue={0}
-                    required
-                  >
-                    <option value={0} disabled>
-                      Selecione aqui
-                    </option>
-                    <OptionsGenerator array={areas} />
-                  </select>
-                </fieldset>
-
-                <fieldset>
-                  <label htmlFor='user-profession'>Profissão</label>
-                  <select
-                    name='user-profession'
-                    id='user-profession'
-                    className='select-appearance'
-                    onChange={handleSelectChange}
-                    defaultValue={0}
-                    required
-                  >
-                    <option value={0} disabled>
-                      Selecione aqui
-                    </option>
-                    <OptionsGenerator array={professions} />
-                  </select>
-
-                  <div className='input-container-research'>
-                    <input type='text' placeholder='Digite aqui' />
-                    <button type='button' onClick={handleInputCancel}>
-                      Cancelar
-                    </button>
-                  </div>
-                </fieldset>
-
-                <fieldset>
-                  <label htmlFor='user-formation'>Formação</label>
-                  <select
-                    name='user-formation'
-                    id='user-formation'
-                    className='select-appearance'
-                    onChange={handleSelectChange}
-                    defaultValue={0}
-                    required
-                  >
-                    <option value={0} disabled>
-                      Selecione aqui
-                    </option>
-                    <OptionsGenerator array={formations} />
-                  </select>
-
-                  <div className='input-container-research'>
-                    <input type='text' placeholder='Digite aqui' />
-                    <button type='button' onClick={handleInputCancel}>
-                      Cancelar
-                    </button>
-                  </div>
-                </fieldset>
-
-                <ButtonRounded buttonStyle='secondary'>
-                  <img src={ProfileIcon} alt='Complete seu perfil' />
-                  Completar meu perfil
-                </ButtonRounded>
-              </form>
-            </div>
-
-            <ProgressBar progress={3} />
-          </div>
+          <CompleteProfileCard />
         </main>
 
         <aside>
