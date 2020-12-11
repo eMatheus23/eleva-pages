fd2509200a1f706e1ccb68438e4c3ec72e94bab1
eMatheus23 <eliasmatheus@hotmail.com>
2020-12-10 22:05:06 -0300

    Criação de card com informações dos cartões
    Componente para o card que mostra os cartões aceitos e oferece a opção
    de salvar o cartão

---
+++ src/components/cards/AcceptedCards.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+
+import '../../styles/components/cards/accepted-cards.css';
+
+import cardImg from '../../assets/images/icons/card-icon-gray.svg';
+import acceptedCardsImg from '../../assets/images/icons/accepted-cards.png';
+import Switch from '@material-ui/core/Switch';
+
+export default function AcceptedCards({ saveCard }) {
+  const [saveCardActive, setSaveCardActive] = useState(false);
+  const [state, setState] = useState({
+    checkedB: false,
+  });
+
+  const handleSwitchChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
+  useEffect(() => {
+    setSaveCardActive(saveCard);
+  }, [saveCard]);
+
+  return (
+    <div className='accepted-cards-container'>
+      <div>
+        <img src={cardImg} alt='' />
+
+        {saveCardActive && (
+          <Switch
+            checked={state.checkedB}
+            onChange={handleSwitchChange}
+            name='checkedB'
+          />
+        )}
+      </div>
+
+      <div>
+        <p>Você pode parcelar em até 10 vezes no cartão de crédito.</p>
+        <img src={acceptedCardsImg} alt='' />
+        {saveCardActive && (
+          <p>Salvar dados do cartão para compras futuras mais ágeis.</p>
+        )}
+      </div>
+    </div>
+  );
+}
