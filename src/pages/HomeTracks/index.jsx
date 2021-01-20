08c32848c5719acb37099d5e12121a72a948be49
eMatheus23 <eliasmatheus@hotmail.com>
2021-01-20 14:58:47 -0300

    Início da página HomeTracks

--- src/pages/HomeTracks/index.jsx
+++ src/pages/HomeTracks/index.jsx
@@ -1,9 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
-import Header from '../../components/headers/MainHeader';
-import Footer from '../../components/footers/MainFooter';
-
-import { Container, Section, Hero } from './styles';
+import { BiSearchAlt } from 'react-icons/bi';
+import { Container, Section, Hero, SearchSection } from './styles';
 
 // Services
 import getViewerStatus from '../../services/getViewerStatus';
@@ -11,7 +9,11 @@ import getViewerStatus from '../../services/getViewerStatus';
 // Icons
 import CoursesIcon from '../../assets/images/icons/courses-icon-02.svg';
 import HeroDetail from '../../assets/images/other/hero-detail.svg';
-import HeroFilter from '../../assets/images/other/Grupo833.png';
+import FilterIcon from '../../assets/images/icons/filter-icon.svg';
+import BitwiseImg from '../../assets/images/icons/bitwise.svg';
+
+import Header from '../../components/headers/MainHeader';
+import Footer from '../../components/footers/MainFooter';
 
 // Mockups
 import HeroImg from '../../assets/images/mockups/home-trilhas/person-touching-open-macbook-on-table-839465.png';
@@ -20,23 +22,23 @@ const HomeTracks = () => {
   const [viewerStatus, setViewerStatus] = useState(getViewerStatus);
 
   // Funções para teste
-  const handleLogin = () => {
+  const handleLogin = useCallback(() => {
     localStorage.setItem('@elevagro-app/viewer-status', 'free');
 
     setViewerStatus('free');
-  };
+  }, []);
 
-  const backToVisit = () => {
+  const backToVisit = useCallback(() => {
     localStorage.setItem('@elevagro-app/viewer-status', 'visit');
 
     setViewerStatus('visit');
-  };
+  }, []);
 
-  const becomePremium = () => {
+  const becomePremium = useCallback(() => {
     localStorage.setItem('@elevagro-app/viewer-status', 'premium');
 
     setViewerStatus('premium');
-  };
+  }, []);
 
   return (
     <Container>
@@ -73,9 +75,21 @@ const HomeTracks = () => {
         </div>
       </Hero>
 
-      <Section>
-        <h2>Main</h2>
-      </Section>
+      <SearchSection>
+        <section>
+          <form className="search-container">
+            <button type="button">
+              <img src={FilterIcon} alt="Filtrar" />
+              CATEGORIA
+              <img src={BitwiseImg} alt="" />
+            </button>
+            <input type="text" placeholder="Localizar uma trilha" />
+            <button type="submit">
+              <BiSearchAlt size={20} />
+            </button>
+          </form>
+        </section>
+      </SearchSection>
 
       <Footer />
     </Container>
--- src/pages/HomeTracks/styles.js
+++ src/pages/HomeTracks/styles.js
@@ -119,3 +119,72 @@ export const Hero = styled.section`
     }
   }
 `;
+
+export const SearchSection = styled.article`
+  width: 100%;
+  max-width: ${wrapperMaxWidth};
+  height: 10.9rem;
+  margin: 4rem auto;
+  padding: 0 4rem;
+
+  form {
+    overflow: hidden;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    width: 100%;
+    max-width: 85.8rem;
+    height: 5.2rem;
+
+    border-radius: 3.2rem;
+    background: var(--color-background-secondary);
+
+    button:first-of-type {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      width: 13.4rem;
+      height: 100%;
+
+      background: none;
+      font: normal 1.3rem/1.6rem var(--font-primary);
+
+      img:first-of-type {
+        width: 1.1rem;
+        margin-left: 2.3rem;
+      }
+    }
+
+    input {
+      flex: 1;
+      display: flex;
+      align-items: center;
+
+      height: 100%;
+
+      padding: 0 3rem;
+
+      background: none;
+      font: normal 1.6rem/1.9rem var(--font-primary);
+
+      &::placeholder {
+        color: #b9b9b9;
+      }
+    }
+  }
+
+  button:last-of-type {
+    display: flex;
+
+    width: 10.4rem;
+    height: 100%;
+    background: var(--color-primary);
+
+    svg {
+      margin: auto;
+      transform: scaleX(-1);
+    }
+  }
+`;
