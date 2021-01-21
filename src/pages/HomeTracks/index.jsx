6db3dd7003b2ba2e92b2c9de35944bc10eff8e84
Elias Oliveira <eliasmatheus@hotmail.com>
2021-01-20 22:25:07 -0300

    Primeiro carrossel finalizado

--- src/pages/HomeTracks/index.jsx
+++ src/pages/HomeTracks/index.jsx
@@ -1,7 +1,19 @@
 import React, { useState, useCallback } from 'react';
+import { Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 
 import { BiSearchAlt } from 'react-icons/bi';
-import { Container, Section, Hero, SearchSection } from './styles';
+import {
+  Container,
+  CarouselContainer,
+  CarouselButton,
+  Hero,
+  SearchSection,
+  TrackCard,
+} from './styles';
 
 // Services
 import getViewerStatus from '../../services/getViewerStatus';
@@ -11,12 +23,17 @@ import CoursesIcon from '../../assets/images/icons/courses-icon-02.svg';
 import HeroDetail from '../../assets/images/other/hero-detail.svg';
 import FilterIcon from '../../assets/images/icons/filter-icon.svg';
 import BitwiseImg from '../../assets/images/icons/bitwise.svg';
+import CardDetail from '../../assets/images/other/card-detail.svg';
 
 import Header from '../../components/headers/MainHeader';
 import Footer from '../../components/footers/MainFooter';
 
 // Mockups
 import HeroImg from '../../assets/images/mockups/home-trilhas/person-touching-open-macbook-on-table-839465.png';
+import PremiumCardImg from '../../assets/images/mockups/home-trilhas/person-in-blue-shirt-wearing-black-round-analog-watch-3747106.png';
+import TrackImg from '../../assets/images/mockups/home-trilhas/Grupo 843.png';
+import TrackImg02 from '../../assets/images/mockups/home-trilhas/soybeans-4019684_960_720.png';
+import TrackImg03 from '../../assets/images/mockups/home-trilhas/thumbnail-10.png';
 
 const HomeTracks = () => {
   const [viewerStatus, setViewerStatus] = useState(getViewerStatus);
@@ -39,6 +56,28 @@ const HomeTracks = () => {
 
     setViewerStatus('premium');
   }, []);
+
+  const responsive = {
+    0: { items: 3 },
+    1100: { items: 4 },
+    1530: { items: 4 },
+  };
+
+  const renderPrevButton = ({ isDisabled }) => {
+    return (
+      <CarouselButton style={{ opacity: isDisabled ? '0.5' : 1 }}>
+        <FiChevronLeft size={30} />
+      </CarouselButton>
+    );
+  };
+
+  const renderNextButton = ({ isDisabled }) => {
+    return (
+      <CarouselButton style={{ opacity: isDisabled ? '0.5' : 1 }}>
+        <FiChevronRight size={30} />
+      </CarouselButton>
+    );
+  };
 
   return (
     <Container>
@@ -75,8 +114,8 @@ const HomeTracks = () => {
         </div>
       </Hero>
 
-      <SearchSection>
-        <section>
+      <SearchSection className="row">
+        <section className="col-7">
           <form className="search-container">
             <button type="button">
               <img src={FilterIcon} alt="Filtrar" />
@@ -88,8 +127,156 @@ const HomeTracks = () => {
               <BiSearchAlt size={20} />
             </button>
           </form>
+
+          <div className="sugestions">
+            <p>Sugestões:</p>
+            <button type="button">SOJA</button>
+            <button type="button">DOENÇAS</button>
+            <button type="button">SEMENTES</button>
+            <button type="button">NUTRIÇÃO</button>
+            <button type="button">DANINHAS</button>
+            <button type="button">SOLOS</button>
+            <button type="button">GESTÃO</button>
+            <button type="button">FINANÇAS</button>
+          </div>
         </section>
+
+        <aside className="col-5">
+          <div>
+            <div className="text">
+              <h4>Descontos Premium</h4>
+              <p>
+                Associado Premium tem grandes descontos e promoções exclusivas
+                em todos as trilhas.
+              </p>
+              <Link to="/">Saiba mais</Link>
+              <img src={CardDetail} alt="" />
+              <div className="triangle" />
+              <div className="filter" />
+            </div>
+
+            <img src={PremiumCardImg} alt="" />
+          </div>
+        </aside>
       </SearchSection>
+
+      <CarouselContainer>
+        <header>
+          <h3>LANÇAMENTOS</h3>
+          <div className="border-bottom" />
+          <Link to="/">VER TODAS</Link>
+        </header>
+
+        <AliceCarousel
+          responsive={responsive}
+          mouseTracking
+          disableDotsControls
+          renderPrevButton={renderPrevButton}
+          renderNextButton={renderNextButton}
+        >
+          <TrackCard>
+            <div>
+              <div>
+                <span>TRILHA</span>
+                <h4>FERRUGEM 360º</h4>
+                <p>Visão completa para o domínio da soja.</p>
+              </div>
+
+              <Link to="/">Saiba mais</Link>
+            </div>
+            <span className="filter" />
+            <img src={TrackImg} alt="" />
+          </TrackCard>
+
+          <TrackCard>
+            <div>
+              <div>
+                <span>TRILHA</span>
+                <h4>VENDAS DE INSUMOS AGRÍCOLAS </h4>
+                <p>
+                  Aumente suas skills de vendas fortaleça o seu argumentario.
+                </p>
+              </div>
+
+              <Link to="/">Saiba mais</Link>
+            </div>
+            <span className="filter" />
+            <img src={TrackImg02} alt="" />
+          </TrackCard>
+
+          <TrackCard>
+            <div>
+              <div>
+                <span>TRILHA</span>
+                <h4>PERCEVEJOS</h4>
+                <p>Conhecimento garantido para o combate.</p>
+              </div>
+
+              <Link to="/">Saiba mais</Link>
+            </div>
+            <span className="filter" />
+            <img src={TrackImg03} alt="" />
+          </TrackCard>
+
+          <TrackCard>
+            <div>
+              <div>
+                <span>TRILHA</span>
+                <h4>FERRUGEM 360º</h4>
+                <p>Visão completa para o domínio da soja.</p>
+              </div>
+
+              <Link to="/">Saiba mais</Link>
+            </div>
+            <span className="filter" />
+            <img src={TrackImg} alt="" />
+          </TrackCard>
+
+          <TrackCard>
+            <div>
+              <div>
+                <span>TRILHA</span>
+                <h4>FERRUGEM 360º</h4>
+                <p>Visão completa para o domínio da soja.</p>
+              </div>
+
+              <Link to="/">Saiba mais</Link>
+            </div>
+            <span className="filter" />
+            <img src={TrackImg} alt="" />
+          </TrackCard>
+
+          <TrackCard>
+            <div>
+              <div>
+                <span>TRILHA</span>
+                <h4>VENDAS DE INSUMOS AGRÍCOLAS </h4>
+                <p>
+                  Aumente suas skills de vendas fortaleça o seu argumentario.
+                </p>
+              </div>
+
+              <Link to="/">Saiba mais</Link>
+            </div>
+            <span className="filter" />
+            <img src={TrackImg02} alt="" />
+          </TrackCard>
+
+          <TrackCard>
+            <div>
+              <div>
+                <span>TRILHA</span>
+                <h4>PERCEVEJOS</h4>
+                <p>Conhecimento garantido para o combate.</p>
+              </div>
+
+              <Link to="/">Saiba mais</Link>
+            </div>
+            <span className="filter" />
+            <img src={TrackImg03} alt="" />
+          </TrackCard>
+        </AliceCarousel>
+      </CarouselContainer>
 
       <Footer />
     </Container>
--- src/pages/HomeTracks/styles.js
+++ src/pages/HomeTracks/styles.js
@@ -121,11 +121,23 @@ export const Hero = styled.section`
 `;
 
 export const SearchSection = styled.article`
+  display: flex;
+  justify-content: space-between;
   width: 100%;
   max-width: ${wrapperMaxWidth};
   height: 10.9rem;
-  margin: 4rem auto;
+  margin: 5rem auto 4rem;
   padding: 0 4rem;
+
+  .col-7 {
+    padding: 0;
+  }
+
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
 
   form {
     overflow: hidden;
@@ -161,11 +173,8 @@ export const SearchSection = styled.article`
       flex: 1;
       display: flex;
       align-items: center;
-
       height: 100%;
-
       padding: 0 3rem;
-
       background: none;
       font: normal 1.6rem/1.9rem var(--font-primary);
 
@@ -173,18 +182,270 @@ export const SearchSection = styled.article`
         color: #b9b9b9;
       }
     }
+
+    button:last-of-type {
+      display: flex;
+      width: 10.4rem;
+      height: 100%;
+      background: var(--color-primary);
+
+      svg {
+        margin: auto;
+        transform: scaleX(-1);
+      }
+    }
   }
 
-  button:last-of-type {
+  .sugestions {
+    overflow-x: hidden;
     display: flex;
+    align-items: center;
+    width: 100%;
+    max-width: 85.8rem;
 
-    width: 10.4rem;
-    height: 100%;
-    background: var(--color-primary);
-
-    svg {
-      margin: auto;
-      transform: scaleX(-1);
+    p {
+      font: normal 1.3rem/1.6rem var(--font-primary);
+      margin-right: 3vw;
     }
+
+    button {
+      padding: 0.3rem 1rem;
+      border: 1px solid var(--color-background-secondary);
+      border-radius: 1.6rem;
+      background: none;
+      font: normal 1.3rem/1.6rem var(--font-primary);
+
+      & + button {
+        margin-left: 0.4vw;
+      }
+    }
+  }
+
+  aside {
+    overflow: hidden;
+    width: 100%;
+    height: 10.9rem;
+    padding-left: 5rem;
+    padding-right: 0;
+    border-radius: 0.9rem;
+
+    > div {
+      position: relative;
+
+      overflow: hidden;
+      width: 100%;
+      height: 100%;
+      border-radius: 0.9rem;
+    }
+
+    .text {
+      position: absolute;
+      z-index: 3;
+      top: 0;
+      right: 0;
+      text-align: right;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      height: 100%;
+      padding: 1.2rem min(2vw, 5rem) 1.1rem 0;
+
+      h4 {
+        font: bold 2.4rem/2.4rem var(--font-primary);
+        color: var(--color-primary);
+      }
+
+      p {
+        max-width: 31.7rem;
+        font: normal 1.4rem/1.6rem var(--font-primary);
+        color: var(--color-text-white);
+      }
+
+      a {
+        font: bold 1.4rem/1.4rem var(--font-primary);
+        color: var(--color-primary);
+      }
+
+      img {
+        position: absolute;
+        right: 36.7rem;
+        top: 0;
+        height: 10.9rem;
+      }
+
+      .triangle {
+        position: absolute;
+        right: 37rem;
+        top: 0;
+        z-index: -1;
+        height: 0;
+        width: 0;
+        border-left: 0px solid transparent;
+        border-right: 18px solid #141414;
+        border-bottom: 55px solid transparent;
+        border-top: 55px solid transparent;
+        opacity: 0.65;
+      }
+
+      .filter {
+        position: absolute;
+        right: 0;
+        top: 0;
+        z-index: -1;
+        height: 10.9rem;
+        width: 37rem;
+        background: #141414;
+        opacity: 0.65;
+      }
+    }
+
+    > img {
+      width: 100%;
+      height: 100%;
+
+      border-radius: 0.9rem;
+
+      object-fit: cover;
+    }
+  }
+`;
+
+export const CarouselContainer = styled.article`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: ${wrapperMaxWidth};
+  margin: 5rem auto 4rem;
+  padding: 0 4rem;
+
+  header {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    margin-bottom: 3rem;
+
+    h3 {
+      margin-right: 1rem;
+      font: bold 2.5rem/2.5rem var(--font-primary);
+    }
+
+    > div {
+      flex: 1;
+      height: 1.6rem;
+      border-bottom: 1px solid #bfbfbf;
+    }
+
+    a {
+      margin-left: 3rem;
+      /* border-bottom: 1px solid #5d883e; */
+      font: normal 2rem/2.5rem var(--font-primary);
+      /* text-decoration: none; */
+      color: #5d883e;
+    }
+  }
+
+  .alice-carousel__prev-btn,
+  .alice-carousel__next-btn {
+    position: absolute;
+    top: 50%;
+    transform: translate(0, -50%);
+    height: 4.8rem;
+    width: 4.8rem;
+    border-radius: 50%;
+    box-shadow: 0px 3px 6px #00000029;
+    border: 1px solid #ffffff;
+    background: var(--color-background);
+    cursor: pointer;
+  }
+
+  .alice-carousel__prev-btn {
+    left: -2rem;
+  }
+
+  .alice-carousel__next-btn {
+    right: -2rem;
+  }
+`;
+
+export const CarouselButton = styled.span`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+`;
+
+export const TrackCard = styled.div`
+  overflow: hidden;
+  position: relative;
+  max-width: 34.8rem;
+  width: 90%;
+  height: 31.9rem;
+  border-radius: 1rem;
+
+  > div {
+    z-index: +2;
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+
+    width: 100%;
+    height: 100%;
+
+    padding: 3rem;
+    color: var(--color-text-white);
+
+    span {
+      font: normal 1.5rem/1.5rem var(--font-primary);
+    }
+
+    h4 {
+      margin-top: 1.5rem;
+      font: bold 2.5rem/2.5rem var(--font-primary);
+    }
+
+    p {
+      font: normal 2rem/2.5rem var(--font-primary);
+    }
+
+    a {
+      border-radius: 2.3rem;
+      padding: 0.8rem 2.5rem;
+      background: var(--color-primary);
+      text-decoration: none;
+      font: normal 1.8rem/2.2rem var(--font-primary);
+      color: #494949;
+      transition: background-color 0.2s;
+
+      &:hover {
+        background: var(--color-primary-hover);
+      }
+    }
+  }
+
+  .filter {
+    z-index: +1;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: #000000;
+    opacity: 0.53;
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    position: absolute;
+    top: 0;
+    left: 0;
   }
 `;
